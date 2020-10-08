b34649e3e3375d9e99a7c25f5edbf544ff22b8e3
ElizabethCalista <elizabethcalissta@gmail.com>
2020-10-08 21:22:23 +0700

    added reservation processed

--- app/src/main/java/platformpbp/uajy/quickresto/MyReservationMenu.java
+++ app/src/main/java/platformpbp/uajy/quickresto/MyReservationMenu.java
@@ -1,28 +1,28 @@
-package platformpbp.uajy.quickresto;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-public class MyReservationMenu extends AppCompatActivity {
-    private FloatingActionButton backmr;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_reservation_menu2);
-
-        backmr = (FloatingActionButton) findViewById(R.id.floating_back_mr);
-        backmr.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MyReservationMenu.this,Home.class);
-                startActivity(intent);
-            }
-        });
-    }
-}
+//package platformpbp.uajy.quickresto;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.View;
+//
+//import com.google.android.material.floatingactionbutton.FloatingActionButton;
+//
+//public class MyReservationMenu extends AppCompatActivity {
+//    private FloatingActionButton backmr;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_my_reservation);
+//
+//        backmr = (FloatingActionButton) findViewById(R.id.floating_back_mr);
+//        backmr.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MyReservationMenu.this,Home.class);
+//                startActivity(intent);
+//            }
+//        });
+//    }
+//}
